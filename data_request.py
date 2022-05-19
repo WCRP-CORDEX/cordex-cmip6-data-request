edc1bf83d30abc25232d47f10d2f261cd942fa43
github action <lars.buntemeyer@hzg.de>
2022-05-19 12:12:34 +0200

    added some cmip6 table helper functions

--- data_request.py
+++ data_request.py
@@ -1,3 +1,8 @@
+# from urllib.request import urlopen
+import json
+import os
+from pathlib import Path
+
 import pandas as pd
 
 sheet_id = "1qUauozwXkq7r1g-L4ALMIkCNINIhhCPx"
@@ -9,12 +14,50 @@ url = (
 
 freqs = ["mon", "day", "6hr", "3hr", "1hr"]
 
+
+table_map = {
+    "mon": "CMIP6_Amon",
+    "day": "CMIP6_day",
+    "6hr": "CMIP6_3hr",
+    "3hr": "CMIP6_3hr",
+    "1hr": "CMIP6_3hr",
+    "fx": "CMIP6_fx",
+}
+
+
 sheet_names = ["Atmos CORE", "Atmos Tier 1", "Atmos Tier 2"]
 
 excel_url = (
     "https://cordex.org/wp-content/uploads/2022/03/"
     + "CORDEX_CMIP6_Atmosphere_Variable_List.xlsx"
 )
+
+
+def cmip6_table_list(only=None):
+    """get list of cmip6 cmor tables from github repo"""
+    from github import Github
+
+    grids = [
+        "CMIP6_CV.json",
+        "CMIP6_coordinate.json",
+        "CMIP6_grids.json",
+        "CMIP6_formula_terms.json",
+    ]
+    drops = []
+    if only is None:
+        only = "variables"
+    if only == "variables":
+        drops = ["CMIP6_input_example.json"] + grids
+    elif only == "grid":
+        return grids
+    g = Github()
+    repo = g.get_repo("PCMDI/cmip6-cmor-tables")
+    contents = repo.get_contents("Tables")
+    return [
+        os.path.basename(c.path)
+        for c in contents
+        if os.path.basename(c.path) not in drops
+    ]
 
 
 def sheet_url(sheet_name):
@@ -60,6 +103,29 @@ def clean_df(df, drop=True):
     return df
 
 
+def get_jsonparsed_data(url):
+    """
+    Cache the content of ``url``, parse it as JSON and return the object.
+    """
+    import pooch
+
+    filename = pooch.retrieve(url, known_hash=None)
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def get_cmip6_cmor_table(table):
+    """get cmip6 cmor table"""
+    table = Path(table).stem
+    if "CMIP6" not in table:
+        table = "CMIP6_" + table
+    url = (
+        "https://raw.githubusercontent.com/PCMDI/cmip6-cmor-tables/"
+        f"master/Tables/{table}.json"
+    )
+    return get_jsonparsed_data(url)
+
+
 def retrieve_data_request(sheet_name=None, skiprows=6, clean=True):
     """retrieve data from cordex data request website
 
@@ -78,3 +144,51 @@ def retrieve_data_request(sheet_name=None, skiprows=6, clean=True):
     if clean is True:
         return clean_df(df)
     return df
+
+
+def get_variable_entry(table, variable):
+    """retrieve variable entry from a CMIP6 cmor table"""
+    return get_cmip6_cmor_table(table)["variable_entry"][variable]
+
+
+def get_variable_entries_by_attributes(table, how=None, **kwargs):
+    """retrieve variable entry from a CMIP6 cmor table
+
+    Returns all variable entries from a CMIP6 cmor table depending
+    on kwargs.
+
+    Parameters
+    ----------
+    table : str
+        CMIP6 cmor table name
+        e.g. 'CMIP_Amon' or 'Amon', ...
+    how : str
+        How to evaluate search results, can be ``"any"`` or ``"all"``.
+        Defaults to ``"all"``
+    **kwargs:
+        Keyword arguments used for searching in variable entry attributes.
+
+    Returns
+    -------
+    results : dict
+        Dictionary of variable entries.
+
+    """
+    if how is None:
+        how = "all"
+    variable_entries = get_cmip6_cmor_table(table)["variable_entry"]
+    results = {}
+    for key, entry in variable_entries.items():
+        founds = [entry[attr] == value for attr, value in kwargs.items()]
+        if eval(f"{how}({founds})") is True:
+            results[key] = entry
+    return results
+
+
+def get_all_variable_entries_by_attributes(how="any", **kwargs):
+    results = {}
+    for t in cmip6_table_list():
+        entries = get_variable_entries_by_attributes(t, how=how, **kwargs)
+        if entries:
+            results[t] = entries
+    return results
