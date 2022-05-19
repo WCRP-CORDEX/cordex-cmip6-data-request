b6279a6b654d53095cd51e9a686740120543002e
github action <lars.buntemeyer@hzg.de>
2022-05-19 14:45:51 +0200

    added __init__.py

---
+++ data_request/__init__.py
@@ -0,0 +1,17 @@
+from data_request import (
+    cmip6_table_list,
+    get_all_variable_entries_by_attributes,
+    get_cmip6_cmor_table,
+    get_variabe_entry,
+    get_variable_entries_by_attributes,
+    retrieve_data_request,
+)
+
+__all__ = [
+    "cmip6_table_list",
+    "get_all_variable_entries_by_attributes",
+    "get_cmip6_cmor_table",
+    "get_variabe_entry",
+    "get_variable_entries_by_attributes",
+    "retrieve_data_request",
+]
