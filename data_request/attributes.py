d4e87ed98def15b885e8b067f2b9dd2ae85fd644
github action <lars.buntemeyer@gmail.com>
2022-07-06 13:33:22 +0200

    commenting out old code

--- data_request/attributes.py
+++ data_request/attributes.py
@@ -63,50 +63,50 @@ def get_value_from_out_name(out_name):
 
 # old routine to copy attributes from CMIP6 tables
 #
-def get_attrs(cmip6, long_name, frequency, out_name):
-    subday = ["1hr", "3hr", "6hr"]
-    subday_pt = ["1hrPt", "3hrPt", "6hrPt"]
-    # attrs = cmip6[(cmip6.long_name == long_name)]
-    var = cmip6[(cmip6.long_name == long_name)]
-    if var.empty:
-        var = cmip6[(cmip6.out_name == out_name)]
-    attrs = var[(var.frequency == frequency)]
-    if len(attrs) == 1:
-        # print(f"found {long_name}, {frequency}")
-        return attrs
-    if len(attrs) > 1:
-        # print(f"not unique: {long_name}, {frequency}, found {len(attrs)}")
-        # print(attrs)
-        attrs = attrs[(attrs.modeling_realm == "atmos")]
-        if len(attrs) == 1:
-            return attrs
-    if attrs.empty:
-        if frequency in subday:
-            for f in subday:
-                attrs = var[(var.frequency == f)]
-                if len(attrs) == 1:
-                    print(f"found {long_name}, {f}, {attrs.frequency.values}")
-                    return attrs
-        if frequency in subday_pt:
-            for f in subday_pt:
-                attrs = var[(var.frequency == f)]
-                if len(attrs) == 1:
-                    print(f"found {long_name}, {f}, {attrs.frequency.values}")
-                    return attrs
-    print(f"nothing found for {long_name}, {out_name}, {frequency}")
-    return attrs
-
-    # result is not unique, try modeling_realm
-
-
-def add_cmip_data(row):
-    attrs = get_attrs(row.long_name, row.frequency, row.out_name)
-    attrs.rename(
-        columns={"frequency": "frequency_cmip6", "out_name": "out_name_cmip6"},
-        inplace=True,
-    )
-    if attrs.empty:
-        return row
-    for c in attrs:
-        row[c] = attrs[c].values[0]
-    return row
+# def get_attrs(cmip6, long_name, frequency, out_name):
+#     subday = ["1hr", "3hr", "6hr"]
+#     subday_pt = ["1hrPt", "3hrPt", "6hrPt"]
+#     # attrs = cmip6[(cmip6.long_name == long_name)]
+#     var = cmip6[(cmip6.long_name == long_name)]
+#     if var.empty:
+#         var = cmip6[(cmip6.out_name == out_name)]
+#     attrs = var[(var.frequency == frequency)]
+#     if len(attrs) == 1:
+#         # print(f"found {long_name}, {frequency}")
+#         return attrs
+#     if len(attrs) > 1:
+#         # print(f"not unique: {long_name}, {frequency}, found {len(attrs)}")
+#         # print(attrs)
+#         attrs = attrs[(attrs.modeling_realm == "atmos")]
+#         if len(attrs) == 1:
+#             return attrs
+#     if attrs.empty:
+#         if frequency in subday:
+#             for f in subday:
+#                 attrs = var[(var.frequency == f)]
+#                 if len(attrs) == 1:
+#                     print(f"found {long_name}, {f}, {attrs.frequency.values}")
+#                     return attrs
+#         if frequency in subday_pt:
+#             for f in subday_pt:
+#                 attrs = var[(var.frequency == f)]
+#                 if len(attrs) == 1:
+#                     print(f"found {long_name}, {f}, {attrs.frequency.values}")
+#                     return attrs
+#     print(f"nothing found for {long_name}, {out_name}, {frequency}")
+#     return attrs
+#
+#     # result is not unique, try modeling_realm
+#
+#
+# def add_cmip_data(row):
+#     attrs = get_attrs(row.long_name, row.frequency, row.out_name)
+#     attrs.rename(
+#         columns={"frequency": "frequency_cmip6", "out_name": "out_name_cmip6"},
+#         inplace=True,
+#     )
+#     if attrs.empty:
+#         return row
+#     for c in attrs:
+#         row[c] = attrs[c].values[0]
+#     return row
